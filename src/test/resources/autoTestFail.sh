d5df0c3a2e152114ce08af4959b001016495c33a
jimmyvuong <jimmy@lcincom.com>
2020-06-07 21:51:02 +0700

    sh file

---
+++ src/test/resources/autoTestFail.sh
@@ -0,0 +1,2 @@
+TEST=$(curl --user "jimmyauto:jimmyvt17" http://52.198.136.239:8080/job/MAVEN_FIVE88_JIMMY/lastBuild/consoleText | grep "FAILURE")
+curl -s -d "chat_id=-459749874&text=${TEST}&disable_web_page_preview=true&parse_mode=markdown" https://api.telegram.org/bot1229608956:AAGPgFoYgcb2nMH0V36vPGEFSO9EOwPU92U/sendMessage > /dev/null
