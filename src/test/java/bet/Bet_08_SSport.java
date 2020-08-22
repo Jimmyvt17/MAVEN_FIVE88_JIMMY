dd4116a9f80aeadaccae3c50b269bd0b051b655a
jimmy <jimmy@lcincom.com>
2020-08-22 14:05:18 +0700

    correct ssport

--- src/test/java/bet/Bet_08_SSport.java
+++ src/test/java/bet/Bet_08_SSport.java
@@ -40,10 +40,7 @@ public class Bet_08_SSport extends BaseTest {
 		log.info("TSport - Step02: Open SSport page\n");
 		sSportPage.openSSportPage("item-sb ssport", "icon-vi-s");
 
-//		log.info("SSport - Step03: Navigate sport iframe to play\n");
-//		sSportPage.navigateToSSportIframe();
-
-		log.info("SSport - Step04: Betting\n");
+		log.info("SSport - Step03: Betting\n");
 		betSSport();
 
 	}
@@ -64,104 +61,108 @@ public class Bet_08_SSport extends BaseTest {
 	}
 
 	protected void betAsiaSport() {
-		boolean i = true;
-		while (i) {
-			List<WebElement> listBet = sSportPage.getListBets(SSportPageUI.betAsiaSSportLocator);
-			log.info("The number of odd is " + listBet.size() + "\n");
+		for (int x = 0; x < 10; x++) {
+			boolean i = true;
+			boolean y = true;
+			while (i) {
+				List<WebElement> listBet = sSportPage.getListBets(SSportPageUI.betAsiaSSportLocator);
+				log.info("The number of odd is " + listBet.size() + "\n");
 
-			int betSelect = randomNumber(listBet.size());
-			log.info("Select odd at order " + betSelect + "\n");
-			try {
-				Thread.sleep(1000);
-			} catch (Throwable e) {
-				e.printStackTrace();
+				int betSelect = randomNumber(listBet.size());
+				log.info("Select odd at order " + betSelect + "\n");
+				try {
+					Thread.sleep(1000);
+				} catch (Throwable e) {
+					e.printStackTrace();
+				}
+
+				String oddBet = sSportPage.getAsiaOddBet(listBet.get(betSelect));
+				log.info("Selected odd is " + oddBet + "\n");
+
+				log.info("Create bet order\n");
+				sSportPage.createBetOder(listBet.get(betSelect));
+
+				log.info("Verify bet oder is created\n");
+				if (sSportPage.isTicketCreated()) {
+					String oddSelect = sSportPage.getOddSelect();
+					log.info("The odd is " + oddSelect + "\n");
+
+					log.info("Verify odd is correct\n");
+					verifyEquals(oddBet, oddSelect);
+
+					log.info("Process betting\n");
+					i = !sSportPage.isAsiaBetSuccess(BET_MONEY);
+					if (i == false) {
+						y = false;
+					}
+				} else {
+					log.info("Order is not created successfully\n");
+					i = true;
+				}
 			}
-
-			String oddBet = sSportPage.getAsiaOddBet(listBet.get(betSelect));
-			log.info("Selected odd is " + oddBet + "\n");
-
-			log.info("Create bet order\n");
-			sSportPage.createBetOder(listBet.get(betSelect));
-
-			log.info("Verify bet oder is created\n");
-			if (sSportPage.isTicketCreated()) {
-				String oddSelect = sSportPage.getOddSelect();
-				log.info("The odd is " + oddSelect + "\n");
-
-				log.info("Verify odd is correct\n");
-				verifyEquals(oddBet, oddSelect);
-
-//				String totalReturn = sSportPage.getReturn();
-//				log.info("Tien thang cuoc = " + totalReturn + "\n");
-//
-//				log.info("Kiem tra tien thang cuoc chinh xac\n");
-//				sSportPage.verifyReturn(totalReturn, oddSelect, BET_MONEY);
-
-				log.info("Process betting\n");
-				i = !sSportPage.isAsiaBetSuccess(BET_MONEY);
-			} else {
-				log.info("Order is not created successfully\n");
-				i = true;
+			if (y == false) {
+				break;
 			}
-
 		}
 
 	}
 
 	protected void betEUSport() {
-		boolean i = true;
-		while (i) {
-			List<WebElement> listBet = sSportPage.getListBets(SSportPageUI.betEuroSSportLocator);
-			log.info("The number of odds is " + listBet.size() + "\n");
+		for (int x = 0; x < 10; x++) {
+			boolean i = true;
+			boolean y = true;
+			while (i) {
+				List<WebElement> listBet = sSportPage.getListBets(SSportPageUI.betEuroSSportLocator);
+				log.info("The number of odds is " + listBet.size() + "\n");
 
-			List<WebElement> listOdd = sSportPage.getListBets(SSportPageUI.betOddEuroSSportLocator);
+				List<WebElement> listOdd = sSportPage.getListBets(SSportPageUI.betOddEuroSSportLocator);
 
-			List<WebElement> listContentBet = sSportPage.getListBets(SSportPageUI.betContentEuroSSportLocator);
+				List<WebElement> listContentBet = sSportPage.getListBets(SSportPageUI.betContentEuroSSportLocator);
 
-			int betSelect = randomNumber((listBet.size() - 1));
-			log.info("Select odd at order " + betSelect + "\n");
-			try {
-				Thread.sleep(1000);
-			} catch (Throwable e) {
-				e.printStackTrace();
+				int betSelect = randomNumber((listBet.size() - 1));
+				log.info("Select odd at order " + betSelect + "\n");
+				try {
+					Thread.sleep(1000);
+				} catch (Throwable e) {
+					e.printStackTrace();
+				}
+
+				String oddBet = sSportPage.getEuroOddBet(listOdd.get(betSelect));
+				log.info("Selected odd is " + oddBet + "\n");
+
+				String contentBet = sSportPage.getContentBet(listContentBet.get(betSelect));
+				log.info("Odd details is " + contentBet + "\n");
+
+				log.info("Create bet order\n");
+				sSportPage.createBetOder(listBet.get(betSelect));
+
+				log.info("Verify bet order is created\n");
+				if (sSportPage.isTicketCreated()) {
+					String oddSelect = sSportPage.getOddSelect();
+					log.info("The odd is " + oddSelect + "\n");
+
+					String contentSelect = sSportPage.getContentSelect();
+					log.info("Order details is " + contentSelect + "\n");
+
+					log.info("Verify odd is correct\n");
+					verifyEquals(oddBet, oddSelect);
+
+					log.info("Verify odd details are correct\n");
+					verifyEquals(contentBet, contentSelect);
+
+					log.info("Process betting\n");
+					i = !sSportPage.isEUBetSuccess(BET_MONEY);
+					if (i == false) {
+						y = false;
+					}
+				} else {
+					log.info("Bet order is not created successfully\n");
+					i = true;
+				}
 			}
-
-			String oddBet = sSportPage.getEuroOddBet(listOdd.get(betSelect));
-			log.info("Selected odd is " + oddBet + "\n");
-
-			String contentBet = sSportPage.getContentBet(listContentBet.get(betSelect));
-			log.info("Odd details is " + contentBet + "\n");
-
-			log.info("Create bet order\n");
-			sSportPage.createBetOder(listBet.get(betSelect));
-
-			log.info("Verify bet order is created\n");
-			if (sSportPage.isTicketCreated()) {
-				String oddSelect = sSportPage.getOddSelect();
-				log.info("The odd is " + oddSelect + "\n");
-
-				String contentSelect = sSportPage.getContentSelect();
-				log.info("Order details is " + contentSelect + "\n");
-
-				log.info("Verify odd is correct\n");
-				verifyEquals(oddBet, oddSelect);
-
-				log.info("Verify odd details are correct\n");
-				verifyEquals(contentBet, contentSelect);
-
-//				String totalReturn = sSportPage.getReturn();
-//				log.info("Tien thang cuoc = " + totalReturn + "\n");
-//
-//				log.info("Kiem tra tien thang cuoc chinh xac\n");
-//				sSportPage.verifyReturn(totalReturn, oddSelect, BET_MONEY);
-
-				log.info("Process betting\n");
-				i = !sSportPage.isEUBetSuccess(BET_MONEY);
-			} else {
-				log.info("Bet order is not created successfully\n");
-				i = true;
+			if (y == false) {
+				break;
 			}
-
 		}
 
 	}
