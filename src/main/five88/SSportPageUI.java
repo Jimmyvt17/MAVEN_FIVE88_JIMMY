b892590144f8e9a9f55771ac06e2c35fd29f8713
jimmy <jimmy@lcincom.com>
2020-10-13 10:42:32 +0700

    fix live bet ssport

--- src/main/five88/SSportPageUI.java
+++ src/main/five88/SSportPageUI.java
@@ -13,7 +13,7 @@ public class SSportPageUI {
     public static By betConfirmSSportLocator = By.xpath("//button[@id='PlaceBetButton']");
     public static By ticketOKSSportLocator = By.xpath("//div[contains(text(),'Đặt cược được chấp nhận')]");
     public static By notEnoughLocator = By.xpath("//span[text()='Số dư không đủ']");
-    public static By betAsiaLiveMatchLocator = By.xpath("//div[@id='asian-view-live-now']");
+    public static By betAsiaLiveMatchLocator = By.xpath("//div[@class='rj-asian-live-now ']");
     public static By betAsiaStreamingVideoLocator = By.xpath("//div[@style='opacity:0.5;width:100%;height:16px;background-repeat:no-repeat;background-position: center;background-image:url(/imgs/i-v2/o/event/streaming.svg)']");
 
     public static By betEuroStreamingVideoLocator = By.xpath("//span[@class='icon i-24-16 event-icons-icon i-o-ev-streaming']");
