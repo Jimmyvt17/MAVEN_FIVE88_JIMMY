4cd15494a31e3b518bc626570b036be1f7a45fe5
jimmy <jimmy@lcincom.com>
2020-12-16 10:16:22 +0700

    correct bet detail

--- src/main/five88/ASportPageUI.java
+++ src/main/five88/ASportPageUI.java
@@ -11,8 +11,8 @@ public class ASportPageUI {
     public static By warningLocator = By.xpath("//div[@class='quickBetPanel']//span[text()='Rất tiếc, giá trị đặt cược của bạn lớn hơn Quỹ Tiền Hiện Có']");
     public static By betBoardASportLocator = By.xpath("//span[text()='Đang diễn ra']");
     public static By ticketOKASportLocator = By.xpath("//div[text()='Đang diễn ra']");
-    public static By betOrderLocator = By.xpath("//div[@class='quickBetPanel']//div[@class='betDetial']");
-    public static By betTicketLocator = By.xpath("//div[@class='betlist__content']//div[@class='betDetial']");
+    public static By betOrderLocator = By.xpath("//div[@class='quickBetPanel']//div[@class='betDetail']");
+    public static By betTicketLocator = By.xpath("//div[@class='betlist__content']//div[@class='betDetail']");
 
     public static String dynamicBetMoney = "//div[@class='quickBetPanel']//div[@title='%s']";
 
