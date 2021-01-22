4caf7da693d2dd1e2873bc303bd317c5548800a2
jimmy <jimmy@lcincom.com>
2021-01-22 20:18:50 +0700

    remove asport and ssport

--- src/test/java/bet/Bet_03_ASport.java
+++ src/test/java/bet/Bet_03_ASport.java
@@ -1,145 +1,132 @@
 package bet;
 
 
-import commons.utility.BaseTest;
-import commons.Constants;
-import commons.PageFactoryManager;
-import commons.reportConfig.ExtentTestManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Parameters;
-import pageObjects.ASportPageObject;
-
-import java.util.List;
-
-public class Bet_03_ASport extends BaseTest {
-
-	WebDriver driver;
-	ASportPageObject aSportPage;
-
-    @Parameters(value = "browser")
-	@BeforeClass
-	public void preConditions(String browserName) {
-
-		driver = openMultiBrowser(browserName, Constants.HOME_URL);
-
-		aSportPage = PageFactoryManager.getASportPage(driver);
-
-	}
-
-	@Override
-	public void Run() {
-		ExtentTestManager.startTest("TC_1_ASport", "TC_1_ASport");
-
-		log.info("ASport - Step01: Login with valid account\n");
-		aSportPage.loginSportAccount();
-
-		log.info("ASport - Step02: Open ASport page\n");
-		aSportPage.openASportPage("item-sb asport", "icon-vi-a");
-
-		log.info("ASport - Step03: Switch to sport iframe to play");
-		aSportPage.switchToASportIframe();
-
-		log.info("ASport - Step04: Betting");
-		betASport();
-
-		log.info("ASport - Step05: Exit iframe\n");
-		aSportPage.quitASportIframe();
-
-		log.info("ASport - Step06: Logout\n");
-		aSportPage.logoutToHomePage();
-
-	}
-
-	protected void betASport() {
-		boolean i = true;
-
-		while (i) {
-			log.info("Now we have " + i + "\n");
-			String beforeBalance = aSportPage.getBalance();
-			log.info("Before balance is " + beforeBalance + "\n");
-
-			List<WebElement> listBet = aSportPage.getBets();
-			log.info("The number of odds is " + listBet.size() + "\n");
-
-			int betSelect = randomNumber(listBet.size());
-			log.info("Select the odd in order " + betSelect + "\n");
-			try {
-				Thread.sleep(1000);
-			} catch (Throwable e) {
-				e.printStackTrace();
-			}
-
-			log.info("Click to open bet panel\n");
-			aSportPage.openBetPanel(listBet.get(betSelect));
-
-			String betOrderDetails = aSportPage.getBetDetails();
-			log.info("Bet details is\n" +betOrderDetails + "\n");
-
-			String BET_MONEY = "50";
-			log.info("Select money to bet = " + BET_MONEY + "\n");
-			aSportPage.selectBetMoney(BET_MONEY);
-
-			log.info("Confirm betting\n");
-			aSportPage.confirmBet();
-
-			log.info("Verify warning text\n");
-			aSportPage.verifyWarningDisplayed();
-
-			log.info("Close bet panel\n");
-			aSportPage.closeWarningDialog();
-			try {
-				Thread.sleep(5 * 1000);
-			} catch (Throwable e) {
-				e.printStackTrace();
-			}
-
-			log.info("Open bet board\n");
-			aSportPage.openBetBoard();
-			try {
-				Thread.sleep(3 * 1000);
-			} catch (Throwable e) {
-				e.printStackTrace();
-			}
-
-			if(aSportPage.isTicketDisplayed()){
-				log.info("Bet ticket in process\n");
-
-				String betTicketDetails = aSportPage.getTicketDetails();
-				log.info("Ticket details is\n" + betTicketDetails + "\n");
-
-				log.info("Verify ticket is correct\n");
-				verifyEquals(betOrderDetails + "\n" + BET_MONEY, betTicketDetails);
-
-				String afterBalance = aSportPage.getBalance();
-				log.info("After balance is " + afterBalance + "\n");
-
-				log.info("Verify balance is updated correct\n");
-				aSportPage.verifyBalanceUpdated(beforeBalance, afterBalance);
-
-				i = false;
-
-				log.info("Then we have " + i + "\n");
-
-			} else {
-
-				i = true;
-				log.info("Then we have " + i + "\n");
-
-				log.info(Constants.betUnsuccessful);
-
-			}
-
-		}
-	}
-
-	@AfterClass(alwaysRun=true)
-	public void afterClass() {
-		
-		closeBrowserAndDriver(driver);
-		
-	}
-
-}
+//public class Bet_03_ASport extends BaseTest {
+//
+//	WebDriver driver;
+//	ASportPageObject aSportPage;
+//
+//    @Parameters(value = "browser")
+//	@BeforeClass
+//	public void preConditions(String browserName) {
+//
+//		driver = openMultiBrowser(browserName, Constants.HOME_URL);
+//
+//		aSportPage = PageFactoryManager.getASportPage(driver);
+//
+//	}
+//
+//	@Override
+//	public void Run() {
+//		ExtentTestManager.startTest("TC_1_ASport", "TC_1_ASport");
+//
+//		log.info("ASport - Step01: Login with valid account\n");
+//		aSportPage.loginSportAccount();
+//
+//		log.info("ASport - Step02: Open ASport page\n");
+//		aSportPage.openASportPage("item-sb asport", "icon-vi-a");
+//
+//		log.info("ASport - Step03: Switch to sport iframe to play");
+//		aSportPage.switchToASportIframe();
+//
+//		log.info("ASport - Step04: Betting");
+//		betASport();
+//
+//		log.info("ASport - Step05: Exit iframe\n");
+//		aSportPage.quitASportIframe();
+//
+//		log.info("ASport - Step06: Logout\n");
+//		aSportPage.logoutToHomePage();
+//
+//	}
+//
+//	protected void betASport() {
+//		boolean i = true;
+//
+//		while (i) {
+//			log.info("Now we have " + i + "\n");
+//			String beforeBalance = aSportPage.getBalance();
+//			log.info("Before balance is " + beforeBalance + "\n");
+//
+//			List<WebElement> listBet = aSportPage.getBets();
+//			log.info("The number of odds is " + listBet.size() + "\n");
+//
+//			int betSelect = randomNumber(listBet.size());
+//			log.info("Select the odd in order " + betSelect + "\n");
+//			try {
+//				Thread.sleep(1000);
+//			} catch (Throwable e) {
+//				e.printStackTrace();
+//			}
+//
+//			log.info("Click to open bet panel\n");
+//			aSportPage.openBetPanel(listBet.get(betSelect));
+//
+//			String betOrderDetails = aSportPage.getBetDetails();
+//			log.info("Bet details is\n" +betOrderDetails + "\n");
+//
+//			String BET_MONEY = "50";
+//			log.info("Select money to bet = " + BET_MONEY + "\n");
+//			aSportPage.selectBetMoney(BET_MONEY);
+//
+//			log.info("Confirm betting\n");
+//			aSportPage.confirmBet();
+//
+//			log.info("Verify warning text\n");
+//			aSportPage.verifyWarningDisplayed();
+//
+//			log.info("Close bet panel\n");
+//			aSportPage.closeWarningDialog();
+//			try {
+//				Thread.sleep(5 * 1000);
+//			} catch (Throwable e) {
+//				e.printStackTrace();
+//			}
+//
+//			log.info("Open bet board\n");
+//			aSportPage.openBetBoard();
+//			try {
+//				Thread.sleep(3 * 1000);
+//			} catch (Throwable e) {
+//				e.printStackTrace();
+//			}
+//
+//			if(aSportPage.isTicketDisplayed()){
+//				log.info("Bet ticket in process\n");
+//
+//				String betTicketDetails = aSportPage.getTicketDetails();
+//				log.info("Ticket details is\n" + betTicketDetails + "\n");
+//
+//				log.info("Verify ticket is correct\n");
+//				verifyEquals(betOrderDetails + "\n" + BET_MONEY, betTicketDetails);
+//
+//				String afterBalance = aSportPage.getBalance();
+//				log.info("After balance is " + afterBalance + "\n");
+//
+//				log.info("Verify balance is updated correct\n");
+//				aSportPage.verifyBalanceUpdated(beforeBalance, afterBalance);
+//
+//				i = false;
+//
+//				log.info("Then we have " + i + "\n");
+//
+//			} else {
+//
+//				i = true;
+//				log.info("Then we have " + i + "\n");
+//
+//				log.info(Constants.betUnsuccessful);
+//
+//			}
+//
+//		}
+//	}
+//
+//	@AfterClass(alwaysRun=true)
+//	public void afterClass() {
+//
+//		closeBrowserAndDriver(driver);
+//
+//	}
+//
+//}
--- src/test/java/bet/Bet_08_SSport.java
+++ src/test/java/bet/Bet_08_SSport.java
@@ -1,145 +1,130 @@
 package bet;
 
-import commons.utility.BaseTest;
-import commons.Constants;
-import commons.PageFactoryManager;
-import commons.reportConfig.ExtentTestManager;
-import five88.SSportPageUI;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Parameters;
-import pageObjects.SSportPageObject;
-
-import java.util.List;
-
-public class Bet_08_SSport extends BaseTest {
-
-	WebDriver driver;
-	SSportPageObject sSportPage;
-
-    @Parameters(value = "browser")
-	@BeforeClass
-	public void preConditions(String browserName) {
-
-		driver = openMultiBrowser(browserName, Constants.HOME_URL);
-
-		sSportPage = PageFactoryManager.getSSportPage(driver);
-
-	}
-
-	@Override
-	public void Run() {
-		ExtentTestManager.startTest("TC_1_SSport", "TC_1_SSport");
-		log.info("SSport - Step01: Login with valid account\n");
-		sSportPage.loginSportAccount();
-
-		log.info("SSport - Step02: Open SSport page\n");
-		sSportPage.openSSportPage("item-sb ssport", "icon-vi-s");
-
-		log.info("SSport - Step03: Betting");
-		betSSport();
-
-		log.info("SSport - Step04: Exit SSport iframe\n");
-		sSportPage.quitSSportIframe();
-
-		log.info("SSport - Step05: Logout\n");
-		sSportPage.logoutToHomePage();
-
-	}
-
-	private void betSSport() {
-		log.info("Switch to SSport iframe");
-		sSportPage.switchToSSportIframe();
-
-		log.info("Bet Asia version");
-		betAsiaSport();
-
-		log.info("Change to Asia live odds\n");
-		sSportPage.changeToLiveBet();
-
-		log.info("Check for Asia streaming video\n");
-		checkForStreamingVideo("Asian view", SSportPageUI.betAsiaStreamingVideoLocator);
-
-		log.info("Change to EU view\n");
-		sSportPage.changeToEUView();
-
-		log.info("Check for EU streaming video\n");
-		checkForStreamingVideo("EU view", SSportPageUI.betEuroStreamingVideoLocator);
-
-	}
-
-	private void betAsiaSport() {
-		for (int x = 0; x <= 5; x++) {
-			if (x < 5) {
-				List<WebElement> listBet = sSportPage.getListBets(SSportPageUI.betAsiaSSportLocator);
-				log.info("The number of odd is " + listBet.size() + "\n");
-
-				int betSelect = randomNumber(listBet.size());
-				log.info("Select odd at order " + betSelect + "\n");
-				try {
-					Thread.sleep(1000);
-				} catch (Throwable e) {
-					e.printStackTrace();
-				}
-
-				String oddBet = sSportPage.getAsiaOddBet(listBet.get(betSelect));
-				log.info("Selected odd is " + oddBet + "\n");
-
-				log.info("Create bet order\n");
-				sSportPage.createBetOder(listBet.get(betSelect));
-
-				log.info("Verify bet oder is created\n");
-				if (sSportPage.isTicketCreated()) {
-					String oddSelect = sSportPage.getOddSelect();
-					log.info("The odd is " + oddSelect + "\n");
-
-					log.info("Verify odd is correct\n");
-					verifyEquals(oddBet, oddSelect);
-
-					log.info("Process betting\n");
-					if (sSportPage.isAsiaBetSuccess("30")) {
-						break;
-					}
-				} else {
-					log.info(Constants.betUnsuccessful);
-				}
-			} else {
-				throw new RuntimeException(Constants.betUnsuccessful);
-			}
-		}
-	}
-
-	private void checkForStreamingVideo(String viewMode, By xPathLocator) {
-		List<WebElement> noVideo = sSportPage.getListBets(xPathLocator);
-		if (noVideo.size() > 0) {
-			log.info("There are " + noVideo.size() + " streaming videos\n");
-
-			int videoSelect = randomNumber(noVideo.size());
-			log.info("Select streaming video at order " + videoSelect + "\n");
-
-			log.info("Open a streaming video\n");
-			sSportPage.createBetOder(noVideo.get(videoSelect));
-
-			log.info("Verify video is displayed\n" + sSportPage.isStreamingVideoDisplayed() + "\n");
-			if (!sSportPage.isStreamingVideoDisplayed()) {
-				throw new RuntimeException(viewMode + "\n" + Constants.streamingVideoError);
-			}
-		} else {
-			log.info(Constants.noStreamingVideo + "\n");
-			sendBot(getClass().getName() + ":\n" + viewMode + "\n" + Constants.noStreamingVideo);
-		}
-
-	}
-
-	@AfterClass(alwaysRun=true)
-	public void afterClass() {
-		
-		closeBrowserAndDriver(driver);
-		
-	}
-
-
-}
+//public class Bet_08_SSport extends BaseTest {
+//
+//	WebDriver driver;
+//	SSportPageObject sSportPage;
+//
+//    @Parameters(value = "browser")
+//	@BeforeClass
+//	public void preConditions(String browserName) {
+//
+//		driver = openMultiBrowser(browserName, Constants.HOME_URL);
+//
+//		sSportPage = PageFactoryManager.getSSportPage(driver);
+//
+//	}
+//
+//	@Override
+//	public void Run() {
+//		ExtentTestManager.startTest("TC_1_SSport", "TC_1_SSport");
+//		log.info("SSport - Step01: Login with valid account\n");
+//		sSportPage.loginSportAccount();
+//
+//		log.info("SSport - Step02: Open SSport page\n");
+//		sSportPage.openSSportPage("item-sb ssport", "icon-vi-s");
+//
+//		log.info("SSport - Step03: Betting");
+//		betSSport();
+//
+//		log.info("SSport - Step04: Exit SSport iframe\n");
+//		sSportPage.quitSSportIframe();
+//
+//		log.info("SSport - Step05: Logout\n");
+//		sSportPage.logoutToHomePage();
+//
+//	}
+//
+//	private void betSSport() {
+//		log.info("Switch to SSport iframe");
+//		sSportPage.switchToSSportIframe();
+//
+//		log.info("Bet Asia version");
+//		betAsiaSport();
+//
+//		log.info("Change to Asia live odds\n");
+//		sSportPage.changeToLiveBet();
+//
+//		log.info("Check for Asia streaming video\n");
+//		checkForStreamingVideo("Asian view", SSportPageUI.betAsiaStreamingVideoLocator);
+//
+//		log.info("Change to EU view\n");
+//		sSportPage.changeToEUView();
+//
+//		log.info("Check for EU streaming video\n");
+//		checkForStreamingVideo("EU view", SSportPageUI.betEuroStreamingVideoLocator);
+//
+//	}
+//
+//	private void betAsiaSport() {
+//		for (int x = 0; x <= 5; x++) {
+//			if (x < 5) {
+//				List<WebElement> listBet = sSportPage.getListBets(SSportPageUI.betAsiaSSportLocator);
+//				log.info("The number of odd is " + listBet.size() + "\n");
+//
+//				int betSelect = randomNumber(listBet.size());
+//				log.info("Select odd at order " + betSelect + "\n");
+//				try {
+//					Thread.sleep(1000);
+//				} catch (Throwable e) {
+//					e.printStackTrace();
+//				}
+//
+//				String oddBet = sSportPage.getAsiaOddBet(listBet.get(betSelect));
+//				log.info("Selected odd is " + oddBet + "\n");
+//
+//				log.info("Create bet order\n");
+//				sSportPage.createBetOder(listBet.get(betSelect));
+//
+//				log.info("Verify bet oder is created\n");
+//				if (sSportPage.isTicketCreated()) {
+//					String oddSelect = sSportPage.getOddSelect();
+//					log.info("The odd is " + oddSelect + "\n");
+//
+//					log.info("Verify odd is correct\n");
+//					verifyEquals(oddBet, oddSelect);
+//
+//					log.info("Process betting\n");
+//					if (sSportPage.isAsiaBetSuccess("30")) {
+//						break;
+//					}
+//				} else {
+//					log.info(Constants.betUnsuccessful);
+//				}
+//			} else {
+//				throw new RuntimeException(Constants.betUnsuccessful);
+//			}
+//		}
+//	}
+//
+//	private void checkForStreamingVideo(String viewMode, By xPathLocator) {
+//		List<WebElement> noVideo = sSportPage.getListBets(xPathLocator);
+//		if (noVideo.size() > 0) {
+//			log.info("There are " + noVideo.size() + " streaming videos\n");
+//
+//			int videoSelect = randomNumber(noVideo.size());
+//			log.info("Select streaming video at order " + videoSelect + "\n");
+//
+//			log.info("Open a streaming video\n");
+//			sSportPage.createBetOder(noVideo.get(videoSelect));
+//
+//			log.info("Verify video is displayed\n" + sSportPage.isStreamingVideoDisplayed() + "\n");
+//			if (!sSportPage.isStreamingVideoDisplayed()) {
+//				throw new RuntimeException(viewMode + "\n" + Constants.streamingVideoError);
+//			}
+//		} else {
+//			log.info(Constants.noStreamingVideo + "\n");
+//			sendBot(getClass().getName() + ":\n" + viewMode + "\n" + Constants.noStreamingVideo);
+//		}
+//
+//	}
+//
+//	@AfterClass(alwaysRun=true)
+//	public void afterClass() {
+//
+//		closeBrowserAndDriver(driver);
+//
+//	}
+//
+//
+//}
